8a2ae8783b79213313fce15c01f12ed4ea6427b9
Brendan Pelto <peltobr1555@uwec.edu>
2023-05-05 11:51:11 -0500

    Style Changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-document.querySelector('.nav-btn').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('nav-open');
-  });
+document.querySelector('.nav-btn').addEventListener('click', function () {
+  const nav = document.querySelector('nav');
+  const googleMap = document.querySelector('#googleMap');
+
+  nav.classList.toggle('nav-open');
+
+  if (nav.classList.contains('nav-open')) {
+    googleMap.style.display = 'none';
+  } else {
+    googleMap.style.display = 'block';
+  }
+});
