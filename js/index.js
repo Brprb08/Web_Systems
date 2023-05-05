b7d9ed12080766f97316d5e634f256bdb36c6cfa
prestoscott <126613551+prestoscott@users.noreply.github.com>
2023-05-05 11:09:58 -0500

    Changing images on home page
    Added a series of pictures that change every couple of seconds. If you wish to change the images displayed or the time between changes, you can edit that in the index.js file.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 document.querySelector('.nav-btn').addEventListener('click', function() {
     document.querySelector('nav').classList.toggle('nav-open');
   });
+
+images = [
+  {"name":"img1", "src":"../images/lure1.jpeg"},
+  {"name":"img1", "src":"../images/lure2.jpeg"},
+  {"name":"img1", "src":"../images/lure3.jpeg"}
+];
+imgIndex = 0;
+
+function changeImg() {
+  document.getElementById("slideshow").src=images[imgIndex].src
+  if (images.length > imgIndex+1){
+    imgIndex++;
+  } else {
+    imgIndex = 0;
+  }
+}
+
+changeImg();
+setInterval(changeImg,2000);
